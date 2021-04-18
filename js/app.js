4b23616788df2db176662af5570b9613fa59407c
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-04-18 16:57:49 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -34,26 +34,26 @@ function BusMall(name, source) {
 
 BusMall.allObject = [];
 
-new BusMall('bag', '../images/bag.jpg');
-new BusMall('banana', '../images/banana.jpg');
-new BusMall('bathroom', '../images/bathroom.jpg');
-new BusMall('boots', '../images/boots.jpg');
-new BusMall('breakfast', '../images/breakfast.jpg');
-new BusMall('bubblegum', '../images/bubblegum.jpg');
-new BusMall('chair', '../images/chair.jpg');
-new BusMall('cthulhu', '../images/cthulhu.jpg');
-new BusMall('dog-duck', '../images/dog-duck.jpg');
-new BusMall('dragon', '../images/dragon.jpg');
-new BusMall('pen', '../images/pen.jpg');
-new BusMall('pet-sweep', '../images/pet-sweep.jpg');
-new BusMall('scissors', '../images/scissors.jpg');
-new BusMall('shark', '../images/shark.jpg');
-new BusMall('sweep', '../images/sweep.png');
-new BusMall('tauntaun', '../images/tauntaun.jpg');
-new BusMall('unicorn', '../images/unicorn.jpg');
-new BusMall('usb', '../images/usb.gif');
-new BusMall('water-can', '../images/water-can.jpg');
-new BusMall('wine-glass', '../images/wine-glass.jpg');
+new BusMall('bag', 'images/bag.jpg');
+new BusMall('banana', 'images/banana.jpg');
+new BusMall('bathroom', 'images/bathroom.jpg');
+new BusMall('boots', 'images/boots.jpg');
+new BusMall('breakfast', 'images/breakfast.jpg');
+new BusMall('bubblegum', 'images/bubblegum.jpg');
+new BusMall('chair', 'images/chair.jpg');
+new BusMall('cthulhu', 'images/cthulhu.jpg');
+new BusMall('dog-duck', 'images/dog-duck.jpg');
+new BusMall('dragon', 'images/dragon.jpg');
+new BusMall('pen', 'images/pen.jpg');
+new BusMall('pet-sweep', 'images/pet-sweep.jpg');
+new BusMall('scissors', 'images/scissors.jpg');
+new BusMall('shark', 'images/shark.jpg');
+new BusMall('sweep', 'images/sweep.png');
+new BusMall('tauntaun', 'images/tauntaun.jpg');
+new BusMall('unicorn', 'images/unicorn.jpg');
+new BusMall('usb', 'images/usb.gif');
+new BusMall('water-can', 'images/water-can.jpg');
+new BusMall('wine-glass', 'images/wine-glass.jpg');
 
 // console.log(BusMall.allObject);
 
